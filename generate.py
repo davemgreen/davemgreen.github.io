4f978fc81d587bf572dd797a1a54d2635e26b22b
David Green <david.green@arm.com>
2026-02-19 09:17:25 +0000

    Add scmp and ucmp

--- generate.py
+++ generate.py
@@ -322,7 +322,7 @@ def do(instr, variant, ty, ty2, extrasize, tyoverride):
 if args.type == 'all' or args.type == 'int':
   def enumint():
     # Int Binops
-    for instr in ['add', 'sub', 'mul', 'and', 'or', 'xor', 'shl', 'ashr', 'lshr', 'sdiv', 'srem', 'udiv', 'urem', 'smin', 'smax', 'umin', 'umax', 'uadd.sat', 'usub.sat', 'sadd.sat', 'ssub.sat', 'rotr', 'rotl', 'clmul']:
+    for instr in ['add', 'sub', 'mul', 'and', 'or', 'xor', 'shl', 'ashr', 'lshr', 'sdiv', 'srem', 'udiv', 'urem', 'smin', 'smax', 'umin', 'umax', 'uadd.sat', 'usub.sat', 'sadd.sat', 'ssub.sat', 'rotr', 'rotl', 'clmul', 'scmp', 'ucmp']:
       for ty in inttypes():
         yield (instr, 'binop', ty, ty, 0, None)
         if instr in ['sdiv', 'srem', 'udiv', 'urem', 'shl', 'ashr', 'lshr', 'rotr', 'rotl']:
